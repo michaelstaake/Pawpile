ece55a12d7429b5aba69d8064f516954b61e2f61
Michael Staake <michael@staake.ai>
2026-05-23 13:41:46 -0700

    Fix error causing npm build failure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -203,7 +203,7 @@ export default function App() {
                     </button>
                   </div>
                 </details>
-              )}
+              ) : null}
             </nav>
           ) : null}
         </header>
